3a35afcebaad5d3c9ff0f28edd3e302007cd8ada
sgcamposano <shamelgcamposano@gmail.com>
2025-06-26 23:00:15 +0800

    push

---
+++ shared/head.php
@@ -0,0 +1,8 @@
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>IskolarWorks</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
+        integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"/>
---
+++ shared/nav.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-dark py-4" style="background-color: #832020;">
+    <div class="container">
+      <a class="navbar-brand fw-bold text-white" href="#">iskolarworks</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item"><a class="nav-link text-white" href="index.php">HOME</a></li>
+          <li class="nav-item"><a class="nav-link text-white" href="about.php">ABOUT</a></li>
+          <li class="nav-item"><a class="nav-link text-white" href="news.php">NEWS</a></li>
+          <li class="nav-item"><a class="nav-link text-white" href="scholarships.php">SCHOLARSHIPS</a></li>
+          <li class="nav-item"><a class="nav-link text-white" href="FAQs.php">FAQs</a></li>
+          <li class="nav-item"><a class="nav-link text-white" href="admin.php"><i class="bi bi-person-circle"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
